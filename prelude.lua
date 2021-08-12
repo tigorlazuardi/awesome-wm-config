f84f5c09c59630fee39557f422d3357e653169d6
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-12 10:16:37 +0700

    triple buffering

--- prelude.lua
+++ prelude.lua
@@ -2,7 +2,7 @@
 
 local exec = os.execute
 exec(
-	'xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI-A-1 --mode 1920x1080 --pos 0x0 --rotate normal'
+	'xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --mode 2560x1440 --pos 1920x0 --rotate normal --set TearFree on --output HDMI-A-1 --mode 1920x1080 --pos 0x0 --rotate normal --set TearFree on'
 )
 
 exec('setxkbmap -option shift:both_capslock_cancel')
