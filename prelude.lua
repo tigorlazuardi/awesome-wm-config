7dded8f3633310cb9790a3f4fb59199dbfb3afbc
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-07-13 22:35:58 +0700

    feat(xrandr): update xrandr conf

--- prelude.lua
+++ prelude.lua
@@ -2,7 +2,7 @@
 
 local exec = os.execute
 exec(
-	'xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --mode 2560x1440 --pos 1920x0 --rotate normal --set TearFree on --output HDMI-A-1 --mode 1920x1080 --pos 0x0 --rotate normal --set TearFree on'
+	'xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DisplayPort-1 --off --output HDMI-A-0 --mode 1920x1080 --pos 4480x0 --rotate normal --output HDMI-A-1 --mode 1920x1080 --pos 0x0 --rotate normal'
 )
 
 exec('setxkbmap -option shift:both_capslock_cancel')
